f6257fc3467dc05c96d1d4eb450769409b2c2c13
Nicole D <nnardella@gmail.com>
2018-09-19 00:09:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
+  if int %3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
 
-  elsif int % 5 == 0
+  elsif int %5 == 0
     "Buzz"
 
   elsif int %3 == 0 && %5 == 0
