459f0c59a4ba4dadb343473e3d9bdd300cfbbffa
Nicole D <nnardella@gmail.com>
2018-09-19 00:20:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -15,17 +15,3 @@ else nil
 
   end
 end
-
-
-def fizzbuzz(int) 
-  if int % 5 == 0 && int % 3 == 0
-    "FizzBuzz"
-  elsif int % 3 == 0
-    "Fizz"
-  elsif int % 5 == 0 
-    "Buzz"
-  else 
-    nil
-  end 
-end 
-
